a53fd118e54a2a649da011ca36c752836b3a9bf7
Lifecycle Research <deploy@apex-empire.com>
2026-05-23 10:52:30 -0600

    Add GitHub Pages deployment workflow

---
+++ out/_next/static/chunks/0-2v-92r0uv10.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/about", [
+  "static/chunks/0rz8.62xogz6k.js",
+  "static/chunks/13~wz~.s4cmex.js",
+  "static/chunks/0i52dw29gh70k.js",
+  "static/chunks/0~fyzd0839aq5.js",
+  "static/chunks/0t-~8-tuf6eco.js",
+  "static/chunks/turbopack-0l~p3vm49.qpz.js"
+])
---
+++ out/_next/static/p0UETSpXgrIWUVgwkHhNM/_buildManifest.js
@@ -0,0 +1,70 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/04~607ag0vohn.js"
+  ],
+  "/_error": [
+    "static/chunks/0vlyjo5bpbzmu.js"
+  ],
+  "/about": [
+    "static/chunks/0-2v-92r0uv10.js"
+  ],
+  "/auth/[[...auth]]": [
+    "static/chunks/0b_u8q~7lv2wb.js"
+  ],
+  "/blog": [
+    "static/chunks/0f5zzhzw7s7bb.js"
+  ],
+  "/blog/[slug]": [
+    "static/chunks/0p60cmg2doi7u.js"
+  ],
+  "/contact": [
+    "static/chunks/0leech836kwce.js"
+  ],
+  "/dashboard": [
+    "static/chunks/0yzsdflv~ux5i.js"
+  ],
+  "/dashboard/billing": [
+    "static/chunks/0a~_.~jz1aea-.js"
+  ],
+  "/dashboard/settings": [
+    "static/chunks/04ihpcjl79k73.js"
+  ],
+  "/docs": [
+    "static/chunks/0q7pjc26-xmt2.js"
+  ],
+  "/pricing": [
+    "static/chunks/0z~.--jko3pe_.js"
+  ],
+  "/privacy": [
+    "static/chunks/0j-7zsbtywx8s.js"
+  ],
+  "/terms": [
+    "static/chunks/16l92yd-i9t_k.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/about",
+    "/api/auth/callback",
+    "/api/auth/[[...auth]]",
+    "/api/create-checkout-session",
+    "/api/stripe-webhook",
+    "/auth/[[...auth]]",
+    "/blog",
+    "/blog/[slug]",
+    "/contact",
+    "/dashboard",
+    "/dashboard/billing",
+    "/dashboard/settings",
+    "/docs",
+    "/pricing",
+    "/privacy",
+    "/terms"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
